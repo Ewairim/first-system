7f23f52c65e991079d8d43f030fc5f6305d1a4e5
quiesscent <ciphedc@gmail.com>
2024-06-26 11:56:55 +0300

    Test Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Nav from './components/Nav';
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+     <Nav/>
+    </>
   )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      The Navbar
+    </div>
+  )
+}
+
+export default Nav
